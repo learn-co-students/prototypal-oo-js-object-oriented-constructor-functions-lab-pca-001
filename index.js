5346196c34b628b55ef5b7434b49642833449b1d
Andrew Kloecker <lt.kvothe@gmail.com>
2020-07-21 11:30:18 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+class Scooter {
+  constructor(year, color, model){
+    this.year = year;
+    this.color = color;
+    this.model = model;
+  }
+}
+
+class Driver {
+  constructor(name, age, experience){
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+  }
+}
+
+class PickupLocation {
+  constructor(addr, city){
+    this.address = addr;
+    this.city = city;
+  }
+}
